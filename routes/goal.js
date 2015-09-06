fa5390ba41bb9914430e5ef114ba47a312332366
aconfee <adamestela@gmail.com>
2015-09-05 22:09:15 -0700

    Goal update edge cases added. Timer over day and week. Clear day and week if new day or week.

--- routes/goal.js
+++ routes/goal.js
@@ -1,4 +1,5 @@
 var Goal = require('../models/Goal');
+var GoalHelper = require('../lib/goalLib');
 
 exports.toggleTimer = function(req, res, next){
 	console.log('Toggling the timer');
@@ -21,22 +22,48 @@ exports.toggleTimer = function(req, res, next){
 			// If the timer has already been started, find the elapsed time and log it.
 			else{
 				var deltaTime = Date.now() - goal.timerStart;
-				console.log('Ending timer with ' + deltaTime + ' miliseconds.');
+				console.log('Ending ' + goal.title + ' timer with ' + deltaTime + ' milliseconds.');
 				
 				var date = new Date();
 				var dateUTC = new Date(date.getTime() - (date.getTimezoneOffset() * 60000));
-				console.log('Time added to: ' + goal.title + '. ' + date.toLocaleString)
 				
+				// Update everything.
 				Goal.update({ _id: goalId }, {
 					$set: { 
 						loggedDayHours: goal.loggedDayHours + (deltaTime / 3600000),
 						loggedWeekHours: goal.loggedWeekHours + (deltaTime / 3600000),
 						loggedTotalHours: goal.loggedTotalHours + (deltaTime / 3600000),
-						timerStart: 0,
-						lastUpdateUTC: dateUTC
+						timerStart: 0
 					}
 				}, 
-				function(){
+				function(){					
+					// Only log current day hours for day log. (If user ended timer on different day than start.)
+					var midnight = new Date(dateUTC.getFullYear(), dateUTC.getMonth(), dateUTC.getDate(), 0,0,0);
+					GoalHelper.newDaySinceLastLogged(goal, function(isNewDay){
+						if(isNewDay){
+							var hoursSinceMidnight = (dateUTC.getTime() - midnight.getTime()) / 3600000;
+							Goal.update({ _id: goalId }, { $set: { loggedDayHours: hoursSinceMidnight, }}, function(){
+								console.log('Past midnight since timer start. Time added: ' + hoursSinceMidnight);
+							});
+						}
+					});
+					
+					// Only log current week hours for week log. (If user ended timer on different week than start).
+					var midnightSunday = new Date(dateUTC.getFullYear(), dateUTC.getMonth(), (dateUTC.getDate() - dateUTC.getDay()), 0,0,0);
+					GoalHelper.newWeekSinceLastLogged(goal, function(isNewWeek){
+						if(isNewWeek){
+							var hoursSinceMidnightSunday = (dateUTC.getTime() - midnightSunday.getTime()) / 3600000;
+							Goal.update({ _id: goalId }, { $set: { loggedDayHours: hoursSinceMidnightSunday, }}, function(){
+								console.log('Past midnight since timer start. Time added: ' + hoursSinceMidnightSunday);
+							});
+						}
+					});
+					
+					// Finished updating. Update last update time.
+					Goal.update({ _id: goalId }, { $set: { lastUpdateUTC: dateUTC, }}, function(){
+						console.log('Goal ' + goal.title + ' updated on ' + date.toLocaleString());
+					});
+					
 					res.redirect('/profile');
 				});
 			}
