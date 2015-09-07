d48fe77df61d6f84583248dab78123d7d38b0eb7
aconfee <adamestela@gmail.com>
2015-09-06 20:53:24 -0700

    Hosted MongoDB database added.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var app = require('./app');
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/1000Hours');
+//var db = mongoose.connect('mongodb://localhost/1000Hours');
+var db = mongoose.connect('mongodb://aconfee:elcoqui18@ds041583.mongolab.com:41583/1000hoursdb')
 
 var port = Number(process.env.PORT || app.get('port'));
 
